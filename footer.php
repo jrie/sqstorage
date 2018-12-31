d5382f255ffa72771a4923608061628ccc5da838
jan@dwrox.net <jan@dwrox.net>
2018-12-31 16:53:28 +0100

    Adding github repository link to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
 <footer class="footer">
     <a class="btn btn-info" tabIndex="-1" href="mailto:jan@dwrox.net"><?php echo gettext('Kontakt') ?></a>
+    <a class="btn btn-info" tabIndex="-1" target="_blank" href="https://github.com/jrie/sqstorage"><?php echo gettext('Github') ?></a>
 </footer>
