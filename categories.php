39744299b66e47f0dd4a6df768623893ed7eb9c1
jan@dwrox.net <jan@dwrox.net>
2019-02-21 06:45:53 +0100

    Fix spelling, fix renaming of items, fix JS code parts, use encodeURIcomponent

--- categories.php
+++ categories.php
@@ -6,14 +6,15 @@
 
         <div class="content">
             <?php
+
                 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     if (isset($_GET['to']) && (isset($_GET['headCategory']) || isset($_GET['subCategory']))) {
                         if (isset($_GET['headCategory'])) {
                             DB::update('headCategories', array('name' => $_GET['to']), 'id=%d', intVal($_GET['headCategory']));
-                            if (DB::affectedRows() === 1) echo '<div class="alert alert-info" role="alert"><p>' . gettext('Kategorie umbennant.') . '</p></div>';
+                            if (DB::affectedRows() === 1) echo '<div class="alert alert-info" role="alert"><p>' . gettext('Kategorie umbenannt.') . '</p></div>';
                         } else {
                             DB::update('subCategories', array('name' => $_GET['to']), 'id=%d', intVal($_GET['subCategory']));
-                            if (DB::affectedRows() === 1) echo '<div class="alert alert-info" role="alert"><p>' . gettext('Unterkategorie umbennant.') . '</p></div>';
+                            if (DB::affectedRows() === 1) echo '<div class="alert alert-info" role="alert"><p>' . gettext('Unterkategorie umbenannt.') . '</p></div>';
                         }
                     } else if (isset($_GET['removeCategory']) && !empty($_GET['removeCategory'])) {
                         DB::delete('headCategories', "id=%d", intVal($_GET['removeCategory']));
@@ -28,13 +29,13 @@
 
                     echo '<hr/><ul class="categories list-group"><li class="alert alert-info"><span class="list-span">' .  gettext('Kategorien') . '</span><span class="list-span">' .  gettext('Anzahl') . '</span><span class="list-span">' .  gettext('Aktionen') . '</span></li>';
                     foreach ($headCategories as $category) {
-                        printf('<li class="list-group-item"><a name="removeCategory" data-name="%s" href="categories.php?removeCategory=%d" class="removalButton fas fa-times-circle btn"></a><a class="list-span" data-name="%s" href="inventory.php?category=%d">%s</a><span class="list-span">%d %s</span><a class="fas fa-edit editCategory" href="categories.php" name="editCategory" data-name="%s" data-id="%d"></a></li>', $category['name'], $category['id'], $category['name'], $category['id'], $category['name'], $category['amount'], $category['amount'] == 1 ? 'Gegenstand' : 'Gegenstände', $category['name'], $category['id']);
+                        printf('<li class="list-group-item"><a name="removeCategory" data-name="%s" href="categories.php?removeCategory=%d" class="removalButton fas fa-times-circle btn"></a><a class="list-span" data-name="%s" href="inventory.php?category=%d">%s</a><span class="list-span">%d %s</span><a class="fas fa-edit editCategory" href="#" name="editCategory" data-name="%s" data-id="%d"></a></li>', $category['name'], $category['id'], $category['name'], $category['id'], $category['name'], $category['amount'], $category['amount'] == 1 ? 'Gegenstand' : 'Gegenstände', $category['name'], $category['id']);
                     }
                     echo '</ul><hr/>';
 
                     echo '<ul class="categories list-group"><li class="alert alert-info"><span class="list-span">' .  gettext('Unterkategorien') . '</span><span class="list-span">' .  gettext('Anzahl') . '</span><span class="list-span">' .  gettext('Aktionen') . '</span></li>';
                     foreach ($subCategories as $category) {
-                        printf('<li class="list-group-item"><a name="removeSubcategory" data-name="%s" href="categories.php?removeSubcategory=%d" class="removalButton fas fa-times-circle btn"></a><a class="list-span" data-name="%s" href="inventory.php?subcategory=%d">%s</a><span class="list-span">%d %s</span><a class="fas fa-edit editCategory" href="categories.php" name="editSubcategory" data-name="%s" data-id="%d"></a></li>', $category['name'], $category['id'], $category['name'], $category['id'], $category['name'], $category['amount'], $category['amount'] == 1 ? 'Gegenstand' : 'Gegenstände', $category['name'], $category['id']);
+                        printf('<li class="list-group-item"><a name="removeSubcategory" data-name="%s" href="categories.php?removeSubcategory=%d" class="removalButton fas fa-times-circle btn"></a><a class="list-span" data-name="%s" href="inventory.php?subcategory=%d">%s</a><span class="list-span">%d %s</span><a class="fas fa-edit editCategory" href="#" name="editSubcategory" data-name="%s" data-id="%d"></a></li>', $category['name'], $category['id'], $category['name'], $category['id'], $category['name'], $category['amount'], $category['amount'] == 1 ? 'Gegenstand' : 'Gegenstände', $category['name'], $category['id']);
                     }
 
                     echo '</ul>';
@@ -48,7 +49,7 @@
             let removalButtons = document.querySelectorAll('.removalButton')
             for (let button of removalButtons) {
                 button.addEventListener('click', function (evt) {
-                    let targetType = evt.target.name === 'removeCategory' ? <?php echo gettext('Kategorie wirklich entfernen?') ?> : <?php echo gettext('Unterkategorie wirklich entfernen?') ?>
+                    let targetType = evt.target.name === 'removeCategory' ? '<?php echo gettext('Kategorie wirklich entfernen?') ?>' : '<?php echo gettext('Unterkategorie wirklich entfernen?') ?>'
                     if (!window.confirm(targetType + ' "' + evt.target.dataset['name'] +'"')) {
                         evt.preventDefault()
                     }
@@ -62,12 +63,10 @@
                     let newName = window.prompt(targetType + ' "' + evt.target.dataset['name'] + '"', '')
 
                     if (newName !== null && newName.length !== 0) {
-                        if (evt.target.name === 'editCategory') window.location.href = 'categories.php?headCategory=' + evt.target.dataset['id'] +  '&to='+ newName
-                        else window.location.href = 'categories.php?subCategory=' + evt.target.dataset['id'] +  '&to='+ newName
-                        return
+                        if (evt.target.name === 'editCategory') window.location.href = 'categories.php?headCategory=' + evt.target.dataset['id'] + '&to=' + encodeURIcomponent(newName)
+                        else window.location.href = 'categories.php?subCategory=' + evt.target.dataset['id'] + '&to=' + encodeURIcomponent(newName)
                     }
 
-                    evt.preventDefault()
                     return false
                 })
             }
