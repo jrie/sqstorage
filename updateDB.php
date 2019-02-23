ac5733f793408ec6f48c4067d61945c5d76c3932
jan@dwrox.net <jan@dwrox.net>
2019-02-23 05:44:24 +0100

    honor null/empty subcategories when updating items

--- updateDB.php
+++ updateDB.php
@@ -1,11 +1,14 @@
 <?php
 include_once('head.php');
 
+DB::$usenull = false;
+
 $items = DB::query('SELECT id, label, subcategories FROM items');
 
 if ($items != null) {
     foreach($items as $item) {
         $subIds = trim($item['subcategories'], ',');
+        if (empty($subIds)) continue;
         $newSubIds = ',' . $subIds . ',';
         DB::update('items', array('subcategories' => $newSubIds), 'id=%d', $item['id']);
         printf('%s - updated...<br/>', $item['label']);
