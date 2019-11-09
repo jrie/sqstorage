c4cceff83488a1a391a097ddbfa6fd83ec5e75d8
jan@dwrox.net <jan@dwrox.net>
2019-11-09 18:04:34 +0100

    Removing early die()

--- updateDB.php
+++ updateDB.php
@@ -1,6 +1,9 @@
 <?php
 include_once('head.php');
 
+DB::query('CREATE TABLE `tlv`.`customFields` ( `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT , `label` VARCHAR(64) NOT NULL , `type` TINYINT UNSIGNED NOT NULL , `defaultValue` VARCHAR(512) NULL DEFAULT NULL , `subvalues` VARCHAR(512) NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci');
+
+
 DB::$usenull = false;
 
 $items = DB::query('SELECT id, label, subcategories FROM items');
