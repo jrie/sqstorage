6397c9eab50e534f1126be8a82e3748af4cf4ae6
jan@dwrox.net <jan@dwrox.net>
2019-12-16 06:38:06 +0100

    Search to be placed on the right side, removing redundant classes

--- nav.php
+++ nav.php
@@ -1,13 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-light bg-light">
     <a href="index.php"><img class="logo" src="./img/sqstorage.png" /></a>
     <ul class="nav">
         <li class="nav-item"><a href="index.php" class="nav-link"><?php echo gettext('Eintragen') ?></a></li>
         <li class="nav-item"><a href="inventory.php" class="nav-link"><?php echo gettext('Inventar') ?></a></li>
         <li class="nav-item"><a href="categories.php" class="nav-link"><?php echo gettext('Kategorien') ?></a></li>
         <li class="nav-item"><a href="transfer.php" class="nav-link"><?php echo gettext('Transferieren') ?></a></li>
+        <li class="nav-item"><a href="datafields.php" class="nav-link"><?php echo gettext('Datenfelder') ?></a></li>
     </ul>
 
-    <form class="form-inline my-2 " method="GET" action="inventory.php">
+    <form class="form-inline searchArea" method="GET" action="inventory.php">
         <input class="form-control mr-sm-2" name="searchValue" type="search" placeholder="<?php echo gettext('Suche') ?>" aria-label="<?php echo gettext('Suche') ?>">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><?php echo gettext('Suchen') ?></button>
     </form>
