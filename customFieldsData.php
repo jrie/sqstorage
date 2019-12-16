57d790d5e400c08bf9e2bbd74428dbc039db54e4
jan@dwrox.net <jan@dwrox.net>
2019-12-16 13:00:14 +0100

    Adding gettext for custom fields texts

--- customFieldsData.php
+++ customFieldsData.php
@@ -11,14 +11,14 @@
     ];
 
     $fieldTypes = [
-        'intNeg' => 'Negative Ganzzahl',
-        'intPos' => 'Positive Ganzzahl',
-        'intNegPos' => 'Negative oder positive Ganzzahl',
-        'floatNeg' => 'Negative oder positive Gleitkommazahl',
-        'floatPos' => 'Positive Gleitkommazahl',
-        'string' => 'Zeichenfolge < 512 Zeichen',
-        'selection' => 'Einzelauswahl (Single-Dropdown)',
-        'mselection' => 'Mehrfachauswahl (Multi-Dropdown)'
+        'intNeg' => gettext('Negative Ganzzahl'),
+        'intPos' => gettext('Positive Ganzzahl'),
+        'intNegPos' => gettext('Negative oder positive Ganzzahl'),
+        'floatNeg' => gettext('Negative oder positive Gleitkommazahl'),
+        'floatPos' => gettext('Positive Gleitkommazahl'),
+        'string' => gettext('Zeichenfolge < 512 Zeichen'),
+        'selection' => gettext('Einzelauswahl (Single-Dropdown)'),
+        'mselection' => gettext('Mehrfachauswahl (Multi-Dropdown)')
     ];
 
     $fieldLimits = [
@@ -33,14 +33,14 @@
     ];
 
     $dataExamples = [
-        '-1' => 'Datentyp Beispielwerte',
-        'intNeg' => 'Einzelwert: -131 oder -7019 oder kleiner gleich 0',
-        'intPos' => 'Einzelwert: 1 oder 291 oder 4016, größer gleich 0',
-        'intNegPos' => 'Einzelwert: -10 oder -2 oder 2000 oder 4000',
-        'floatNeg' => 'Einzelwert: -11.4 oder -0.032 kleiner gleich 0',
-        'floatPos' => 'Einzelwert: 3.4172 oder 5.179 oder 27.4',
-        'string' => 'Einzelwert: Zeichenfolge mit bis zu < 512 Zeichen',
-        'selection' => 'Einzelauswahl, durch Komma getrennt: erlaubt "Neu" oder "Gebraucht" oder "Refurbished" wenn "Neu,Gebraucht,Refurbished" angegeben. Und oder numerische Werte (Ganzzahl, Gleitkommazahlen)',
-        'mselection' => 'Mehrfachauswahl, durch Komma getrennt: erlaubt "Neu" und oder "Netzwerk", wenn "Neu,Netzwerk" angegeben. Und oder numerische Werte (Ganzzahl, Gleitkommanzahlen).'
+        '-1' => gettext('Datentyp Beispielwerte'),
+        'intNeg' => gettext('Einzelwert: -131 oder -7019 oder kleiner gleich 0'),
+        'intPos' => gettext('Einzelwert: 1 oder 291 oder 4016, größer gleich 0'),
+        'intNegPos' => gettext('Einzelwert: -10 oder -2 oder 2000 oder 4000'),
+        'floatNeg' => gettext('Einzelwert: -11.4 oder -0.032 kleiner gleich 0'),
+        'floatPos' => gettext('Einzelwert: 3.4172 oder 5.179 oder 27.4'),
+        'string' => gettext('Einzelwert: Zeichenfolge mit bis zu < 512 Zeichen'),
+        'selection' => gettext('Einzelauswahl, durch Komma getrennt: erlaubt "Neu" oder "Gebraucht" oder "Refurbished" wenn "Neu,Gebraucht,Refurbished" angegeben. Und oder numerische Werte (Ganzzahl, Gleitkommazahlen)'),
+        'mselection' => gettext('Mehrfachauswahl, durch Komma getrennt: erlaubt "Neu" und oder "Netzwerk", wenn "Neu,Netzwerk" angegeben. Und oder numerische Werte (Ganzzahl, Gleitkommanzahlen).')
     ];
 ?>
