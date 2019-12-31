0f0f9fd3ad1ef21061e7898c643ed91a9e125d9f
schnoog <braun.volker@gmail.com>
2019-12-31 10:43:43 +0100

    Erste Dateien von oldsqstorage transferiert, verzeichnis-struktur mit touchfiles aufgebaut

---
+++ includer.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$basedir = __DIR__;
+require_once($basedir .'/support/meekrodb.2.3.class.php');
+require_once($basedir.'/support/dba.php'); 
+require_once($basedir.'/vendor/autoload.php');
+
+require_once($basedir.'/support/language_tools.php');
+require_once($basedir.'/support/smartyconfig.php');
+
