b2b637830e4d5bdf23025e38f1d33a82bb20b4e9
Schnoog <schnoog@schnoog.eu>
2020-01-02 18:18:34 +0100

    Language-Extraxt-weiter-treiben

--- php_gettext_extract.sh
+++ php_gettext_extract.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-src="')"
-rpl="\n"
+MYIFS=$IFS
+IFS="
+"
 
-grep gettext *.php | sed -e "s/\n//g"  |  sed -e "s/gettext/\ngettext/g" | grep gettext
+TARGETTEMPLATE="templates/php_language_extract.tpl"
+
+
+ALL=$(grep gettext *.php | sed -e "s/\n//g"  |  sed -e "s/gettext/\ngettext/g" | grep "gettext('" | cut -d "'" -f 2 | sort -u)
+
+for PHRASE in $ALL
+do
+
+term='{t}'"$PHRASE"'{/t}'
+if [ $(grep "$term" "$TARGETTEMPLATE" | wc -l) -eq 0 ]
+then
+
+echo "$term" 
+echo "$term" >> "$TARGETTEMPLATE"
+
+fi
+
+
+
+
+
+
+done
+IFS=$MYIFS
