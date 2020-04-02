1cf2c2064c144465163a23816dd94ca42deb0c45
jan@dwrox.net <jan@dwrox.net>
2020-04-02 20:28:55 +0200

    Cutting / from $urlBase variable

--- support/urlBase.php
+++ support/urlBase.php
@@ -1,2 +1,2 @@
 <?php
-    $urlBase = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['[REQUEST_URI'] . dirname($_SERVER['SCRIPT_NAME']);
+    $urlBase = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . rtrim($_SERVER['[REQUEST_URI'], '/') . dirname($_SERVER['SCRIPT_NAME']);
