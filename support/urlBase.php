5bf19b1e9508a61ed695f3f086c024fed65a0674
jan@dwrox.net <jan@dwrox.net>
2020-04-02 19:36:18 +0200

    Improving relative url build up in templates and scripts

--- support/urlBase.php
+++ support/urlBase.php
@@ -1,2 +1,2 @@
 <?php
-    $urlBase = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
+    $urlBase = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['[REQUEST_URI'] . dirname($_SERVER['SCRIPT_NAME']);
