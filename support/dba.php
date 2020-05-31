63ea5d958603d865fef5a4f8941e3e283658ff2d
jan@dwrox.net <jan@dwrox.net>
2020-05-31 12:12:06 +0200

    Removing dba.php from repository in favor of dba-example.php

--- support/dba.php
+++
@@ -1,8 +0,0 @@
-<?php
-    DB::$user = 'tlvUser';
-    DB::$password = 'tlvUser';
-    DB::$dbName = 'tlv';
-    DB::$encoding = 'utf8';
-    DB::$host = 'localhost';
-    // DB::$host = '123.111.10.23'; // Use for webserver
-    DB::$port = '3306';
