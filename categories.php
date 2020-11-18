06069431fda4ec5b0d8c4ca1a37c0869fb493f17
jan@dwrox.net <jan@dwrox.net>
2020-11-18 07:22:00 +0100

    Bugfix for wrong count after subcategory was removed

--- categories.php
+++ categories.php
@@ -53,6 +53,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     DB::delete('headCategories', "id=%d", intval($_GET['removeCategory']));
     if (DB::affectedRows() === 1) $alert = '<div class="alert alert-info" role="alert"><p>' . gettext('Kategorie entfernt.') . '</p></div>';
   } else if (isset($_GET['removeSubcategory']) && !empty($_GET['removeSubcategory'])) {
+    $subCategory = DB::queryFirstRow('SELECT `id`, `amount`, `headcategory` FROM `subCategories` WHERE id=%d', intval($_GET['removeSubcategory']));
+    $previousCategory = DB::queryFirstRow('SELECT `id`, `amount` FROM `headCategories` WHERE `id`=%d',  $subCategory['headcategory']);
+    DB::update('headCategories', array('amount' => intval($previousCategory['amount']) - $subCategory['amount']), 'id=%d',  $previousCategory['id']);
+
     DB::delete('subCategories', "id=%d", intval($_GET['removeSubcategory']));
     if (DB::affectedRows() === 1) $alert = '<div class="alert alert-info" role="alert"><p>' . gettext('Unterkategorie entfernt.') . '</p></div>';
   }
