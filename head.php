f49bd154599da149a19c2325e7a3326cb5d1bd2c
jan@dwrox.net <jan@dwrox.net>
2022-05-29 16:17:12 +0200

    Adding favicon.ico and include in head

--- head.php
+++ head.php
@@ -2,10 +2,10 @@
   <title>sqStorage</title>
   <link rel="stylesheet" href="./css/bootstrap/bootstrap.css">
   <link rel="stylesheet" href="./css/base.css">
-
   <link rel="stylesheet" href="./fonts/fontawesome/css/solid.css">
   <link rel="stylesheet" href="./fonts/fontawesome/css/regular.css">
   <link rel="stylesheet" href="./fonts/fontawesome/css/fontawesome.css">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php
--- templates/head.tpl
+++ templates/head.tpl
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./fonts/fontawesome/css/solid.css">
     <link rel="stylesheet" href="./fonts/fontawesome/css/regular.css">
     <link rel="stylesheet" href="./fonts/fontawesome/css/fontawesome.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
