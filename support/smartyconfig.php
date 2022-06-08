6ff6f274cd86c6cea439534137f42a03061ad136
jan@dwrox.net <jan@dwrox.net>
2022-06-08 04:52:53 +0200

    Fix session active check

--- support/smartyconfig.php
+++ support/smartyconfig.php
@@ -13,7 +13,9 @@ if (isset($langsAvailable)) {
   $smarty->assign('langCurrent', $langCurrent);
 }
 
-if (isset($_SESSION)) $smarty->assign('SESSION', $_SESSION);
+if (session_status() == PHP_SESSION_ACTIVE) {
+  $smarty->assign('SESSION', $_SESSION);
+}
 
 //** un-comment the following line to show the debug console
 //$smarty->debugging = true;
