4de79762ecf8b0eacaf385bb3546ba6d6f411771
jan@dwrox.net <jan@dwrox.net>
2022-07-30 17:29:58 +0200

    Updating transfer... to include copy of images on partial transfer

--- transfer.php
+++ transfer.php
@@ -37,30 +37,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $transferAmounts = explode(',', trim($_GET['transferAmounts'], '"'));
 
     foreach ($transferIds as $index => $itemId) {
-      $item = DB::queryFirstRow('SELECT * FROM items WHERE id=%d', $itemId);
+      $item = DB::queryFirstRow('SELECT * FROM items WHERE id=%d LIMIT 1', $itemId);
       if ($item === null) continue;
-      $srcStorage = DB::queryFirstRow('SELECT id, amount FROM storages WHERE id=%d', $item['storageid']);
-      $destStorage = DB::queryFirstRow('SELECT id, amount FROM storages WHERE id=%d', $targetStorageId);
-      $existingDest = DB::queryFirstRow('SELECT id, amount FROM items WHERE label=%s AND storageid=%d', $item['label'], $destStorage['id']);
+      $srcStorage = DB::queryFirstRow('SELECT id, amount FROM storages WHERE id=%d LIMIT 1', $item['storageid']);
+      $destStorage = DB::queryFirstRow('SELECT id, amount FROM storages WHERE id=%d LIMIT 1', $targetStorageId);
+      $existingDest = DB::queryFirstRow('SELECT id, amount FROM items WHERE label=%s AND storageid=%d LIMIT 1', $item['label'], $destStorage['id']);
+      $images = DB::query('SELECT * FROM images WHERE itemId=%d', $itemId);
 
       $leftAmount = (int)$item['amount'] - (int)$transferAmounts[$index];
-      if ($leftAmount != 0) DB::update('items', array('amount' => $leftAmount), "id=%d", $itemId);
+      if ($leftAmount != 0) {
+        DB::update('items', array('amount' => $leftAmount), "id=%d", $itemId);
+      }
 
       if ($existingDest === null) {
         $insertarray = array();
-        foreach ($item as $key => $value) if ($key != 'id') $insertarray[$key] = $value;
+        
+        foreach ($item as $key => $value) {
+          if ($key !== 'id') {
+            $insertarray[$key] = $value;
+          }
+        }
+
         $insertarray['storageid'] = (int)$destStorage['id'];
         $insertarray['amount'] = (int)$transferAmounts[$index];
         DB::insert("items", $insertarray);
+        
+        $insertedId = DB::insertId();
+        foreach ($images as $image) {
+          unset($image['id']);
+          $image['itemId'] = $insertedId;
+          DB::insert('images', $image);
+        }
       } else {
-        DB::update('items', array('amount' => $existingDest['amount'] + (int)$transferAmounts[$index]), "id=%d", $existingDest['id']);
-        DB::update('images', array('itemId' => $existingDest['id']), "id=%d", $itemId);
+        DB::update('items', array('amount' => $existingDest['amount'] + (int)$transferAmounts[$index]), 'id=%d', $existingDest['id']);
+        DB::update('images', array('itemId' => $existingDest['id']), 'itemId=%d', $itemId);
 
         // NOTE: Add extra check to add/rewrite field data to target merge item or to delete field data instead?
         //DB::update('fieldData', array('itemId' => $existingDest['id']), "id=%d", $itemId);
-        DB::delete('fieldData', "id=%d", $itemId);
+        DB::delete('fieldData', 'id=%d', $itemId);
       }
-      if ($leftAmount === 0) DB::delete('items', "id=%d", $item['id']);
+
+      if ($leftAmount === 0) {
+        DB::delete('items', 'id=%d', $item['id']);
+      }
 
       DB::update('storages', array('amount' => (int)$srcStorage['amount'] - (int)$transferAmounts[$index]), 'id=%d', $srcStorage['id']);
       DB::update('storages', array('amount' => (int)$destStorage['amount'] + (int)$transferAmounts[$index]), 'id=%d', $destStorage['id']);
