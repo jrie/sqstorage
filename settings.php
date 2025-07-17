0daaad82acf99805c299333b8cc1dcd7867f6946
Jan Riechers <jan@dwrox.net>
2025-07-17 14:01:32 +0200

    Fix user addition from within settings page
    Add missing "user.api_access" retrival in meekrodb statement to allow user addition and fixes a incorrect display of the database requiring a update

--- settings.php
+++ settings.php
@@ -187,11 +187,12 @@ if ($mailDB !== NULL) {
   $mailSettings['enabled'] = false;
 }
 
-if(!in_array('failcount',DB::columnList('users'))){
-  $users = DB::query('SELECT u.id, u.username, u.mailaddress, g.name as usergroupname, g.id as usergroupid FROM users u LEFT JOIN users_groups ugs ON(ugs.userid = u.id) LEFT JOIN usergroups g ON(g.id = ugs.usergroupid) ORDER BY u.username ASC');
-} else {
-  $users = DB::query('SELECT u.id, u.username, u.mailaddress, u.api_access, g.name as usergroupname, g.id as usergroupid FROM users u LEFT JOIN users_groups ugs ON(ugs.userid = u.id) LEFT JOIN usergroups g ON(g.id = ugs.usergroupid) ORDER BY u.username ASC');
-}
+//if(!in_array('failcount',DB::columnList('users'))){
+//  $users = DB::query('SELECT u.id, u.username, u.mailaddress, u.api_access, g.name as usergroupname, g.id as usergroupid FROM users u LEFT JOIN users_groups ugs ON(ugs.userid = u.id) LEFT JOIN usergroups g ON(g.id = ugs.usergroupid) ORDER BY u.username ASC');
+//} else {
+//  $users = DB::query('SELECT u.id, u.username, u.mailaddress, u.api_access, g.name as usergroupname, g.id as usergroupid FROM users u LEFT JOIN users_groups ugs ON(ugs.userid = u.id) LEFT JOIN usergroups g ON(g.id = ugs.usergroupid) ORDER BY u.username ASC');
+//}
+$users = DB::query('SELECT u.id, u.username, u.mailaddress, u.api_access, g.name as usergroupname, g.id as usergroupid FROM users u LEFT JOIN users_groups ugs ON(ugs.userid = u.id) LEFT JOIN usergroups g ON(g.id = ugs.usergroupid) ORDER BY u.username ASC');
 
 $dbUpdateAvailable = IsDBUpdateAvailable();
 $pages = [
