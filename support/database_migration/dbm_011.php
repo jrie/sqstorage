2d6574d091743400f4df5defa96f556073a4e59f
jrie <jan@dwrox.net>
2026-06-28 00:12:33 +0200

    Database (mysql) "fieldData.datetime" allowing "null" value
    Resolves #114 (thanks @Foxconn11 for report/suggestion/fix)

--- support/database_migration/dbm_011.php
+++ support/database_migration/dbm_011.php
@@ -1,6 +1,6 @@
 <?php
 $customFieldRev = 2;
-DB::query('ALTER TABLE `fieldData` ADD `datetime` DATETIME NOT NULL AFTER `qrcode`;');
+DB::query('ALTER TABLE `fieldData` ADD `datetime` DATETIME NULL AFTER `qrcode`;');
 DB::query('ALTER TABLE `fieldData` ADD `floatNegPos` DOUBLE NULL DEFAULT NULL AFTER `floatPos`;');
 DB::query('ALTER TABLE `database_rev` ADD `customfieldrev` BIGINT UNSIGNED NOT NULL DEFAULT 1 AFTER `dbrev`;');
 
